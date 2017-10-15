c12f2a0e5a276f3a3ed0183b66e6de158145af56
chienwei <chienwei0526@sina.com>
2017-10-16 00:22:06 +0800

    starbase

---
+++ starbase/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+	<div id="app">
+		<h3>{{title}}</h3>
+		<div v-for="id in initialids" class="col-md-12">
+		  <char :id="id"/>
+		</div>
+	</div>
+</template>
+
+<script>
+import char from './Char.vue'
+
+	export default{
+		name: 'app',
+		data(){
+			return{
+				title: 'Generate Your Teams',
+				initialids : [1,13,14]
+			}
+		},
+		components:{
+			char
+		}
+	}
+</script>
+
+<style lang="sass">
+body
+  background: #263238
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: white
+  margin-top: 60px
+
+.character-card
+  border: 2px solid #4FC08D
+  cursor: pointer
+  border-radius: 4px
+</style>
---
+++ starbase/src/Char.vue
@@ -0,0 +1,43 @@
+<template>
+	<div  @click="switchChar()">
+		<div class="character-card">
+			<div class="card-block">
+				<h4 class="card-title">{{charactar.name}}</h4>
+				<p class="card-text">Height: {{charactar.height}}</p>
+				<p class="card-text">Mass: {{charactar.mass}}</p>
+				<p class="card-text">Hair Color: {{charactar.hair_color}}</p>
+				<p class="card-text">Eye Color: {{charactar.eye_color}}</p>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+export default{
+	props: ['id'],
+	data(){
+		return{
+			charactar: {}
+		}
+	},
+	methods:{
+		fetchChar(id){
+		  	fetch(`http://swapi.co/api/people/${id}`,{
+		  		method: 'GET',
+		  		mode: 'cors'
+		  	})
+		  	.then(response => response.json())
+		  	.then(json => this.charactar = json);
+		  },
+		switchChar(){
+			let random_id =Math.floor(Math.random()*83)+1
+			this.fetchChar(random_id)
+		}
+	},
+	created(){
+		this.fetchChar(this.id)
+	}
+}
+</script>
+<style>
+	
+</style>
